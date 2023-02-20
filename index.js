df498f9ef97776ab287cd21122173bfcc4bf941a
Peter Yelton <racknahm@gmail.com>
2023-02-20 08:23:37 -0500

    Created loop arrays

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const { getModulesPluginNames } = require("@babel/preset-env");
+
 // Code your solutions in this file
+const friends =["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(friends) {
+    let cards = []
+  for (let i = 0; i < friends.length; i++) {
+    cards.push(`Thank you, ${friends[i]}, for the wonderful surprise gift!`);
+   }
+   return cards
+}
+function countDown(){
+for (let count = 10; count > 0-1; count--) {
+    console.log(count)
+}
+}
+ 
